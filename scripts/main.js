23b82787bea96f7ba191f562a0c9d8b94016fdb7
SaszaH <herasimchyk.a@gmail.com>
2022-07-16 13:42:35 +0200

    exercise-03 - for, Math.random,floor,ceil, function between

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,26 @@
-//Exe 1
-//100 - srebrny
-//1000 - złoty
-//10000 - diamentowy
+// Losowanie 2h liczb od 0 do 10
+// Zapisujemy poprawna odpowiedz i pytamy o wynik
+// Czy podany wynik jest poprawny
 
-const userSub = parseInt(prompt("How many subscribes do You have"));
-const userMonthlySub = parseInt(prompt("How many subscribiers do you get monthly?"));
+// 10 pytan
+// Ilosc poprawnych odpowiedzi
 
-if (isNaN(userSub) === false && isNaN(userMonthlySub) === false) {
-    
-    const userYearlySub = userMonthlySub * 12;
-    const userSubNextYear = userSub + userYearlySub;
-    console.log(`Your subscriptions number next year will be ${userSubNextYear}`);
-
-    if(userSub >= 100 && userSub < 1000) {
-        console.log("You have silver button");
-
-    } else if(userSub >= 1000 && userSub < 10000) {
-        console.log("You have gold button");
-
-    } else if(userSub >= 10000) {
-        console.log("You have diamond button");
-
-    }  else {
-        const needSub = 100 - userSub;
-        console.log(`You need ${needSub} to reach the silver button`);
-    }
-
-} else {
-    console.log('please provide valid number');
+function between (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+let correctAnswersNumb = 0;
+
+for (let i = 0; i < 3; i++) {
+    const firstRandomNumb = between(0, 10);
+    const secondRandomNumb = between(0, 10);
+    const correctResult = firstRandomNumb * secondRandomNumb;
+    const userNumb = parseInt(prompt(`How much is ${firstRandomNumb} * ${secondRandomNumb}`));
+    if (userNumb === correctResult) {
+        correctAnswersNumb++;
+    }
+}
+console.log(`You have ${correctAnswersNumb} correct answers`)
 
 
 
