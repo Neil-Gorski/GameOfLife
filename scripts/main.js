3db17e539999e7f34a16cba69c11b3486b159d73
Magru <maciej.karol@gmail.com>
2022-07-16 12:19:29 +0200

    exercise-02 parseInt added, isNan, subscriptions next year

--- scripts/main.js
+++ scripts/main.js
@@ -3,21 +3,32 @@
 //1000 - złoty
 //10000 - diamentowy
 
+const userSub = parseInt(prompt("How many subscribes do You have"));
+const userMonthlySub = parseInt(prompt("How many subscribiers do you get monthly?"));
 
-const userSub = prompt("How many subscribes do You have");
-console.log(userSub);
-if(userSub >= 100 && userSub < 1000) {
-    console.log("You have silver button");
+if (isNaN(userSub) === false && isNaN(userMonthlySub) === false) {
+    
+    const userYearlySub = userMonthlySub * 12;
+    const userSubNextYear = userSub + userYearlySub;
+    console.log(`Your subscriptions number next year will be ${userSubNextYear}`);
 
-} else if(userSub >= 1000 && userSub < 10000) {
-    console.log("You have gold button");
+    if(userSub >= 100 && userSub < 1000) {
+        console.log("You have silver button");
 
-} else if(userSub >= 10000) {
-    console.log("You have diamond button");
+    } else if(userSub >= 1000 && userSub < 10000) {
+        console.log("You have gold button");
 
-}  else {
-    const needSub = 100 - userSub;
-    console.log(`You need ${needSub} to reach the silver button`);
+    } else if(userSub >= 10000) {
+        console.log("You have diamond button");
 
+    }  else {
+        const needSub = 100 - userSub;
+        console.log(`You need ${needSub} to reach the silver button`);
+    }
+
+} else {
+    console.log('please provide valid number');
 }
+
+
 
