aee82091f36a76f9c85f24442c6a01a842470372
Erwin Heba <erwin.heba@twoupdigital.com>
2022-07-16 10:59:35 +0200

    exercise-2 - First exe - instruction

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,5 @@
-//strings
-"" <-
-''
-``
+//Exe 1
+//100 - srebrny
+//1000 - złoty
+//10000 - diamentowy
 
-const num = 10;
-
-"Primitive type " + num + " code:";
-`Primitive type ${num} code:`;
-
-// -------------------
-//numbers
-10
-10.2
--2
-
-// -------------------
-// boolean
-true/false
-
-// -------------------
-null
-undefined
-
-let firstPerson = null;
-firstPerson = {};
-
-// -------------------
-// array
-[1, 'name', {name: 'adad'}, [1,2,3], () => {}];
-
-// object
-{
-    name: 'adad'
-}
