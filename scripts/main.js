26749c8264a8eb769e1b2fdbef166146f4be4652
8persistent <8persistent@gmail.com>
2022-07-16 11:43:56 +0300

    Exercise-02 - prompt, if, else, operatory AND i OR

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,21 @@
 //1000 - złoty
 //10000 - diamentowy
 
+
+const userSub = prompt("How many subscribes do You have");
+console.log(userSub);
+if(userSub >= 100 && userSub < 1000) {
+    console.log("You have silver button");
+
+} else if(userSub >= 1000 && userSub < 10000) {
+    console.log("You have gold button");
+
+} else if(userSub >= 10000) {
+    console.log("You have diamond button");
+
+}  else {
+    const needSub = 100 - userSub;
+    console.log(`You need ${needSub} to reach the silver button`);
+
+}
+
