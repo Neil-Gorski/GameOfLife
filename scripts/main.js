67aa6cbbb9ae7453d9e907b573454f2179ffa109
Leszek <lechu73@gmail.com>
2022-07-16 14:37:58 +0200

    exercise-03 - function, while, isNaN

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,21 @@
 // 10 pytan
 // Ilosc poprawnych odpowiedzi
 
+//Ile  ma być pytań
+//Pytamy tyle razy aż  użytkownik poda odpowiednią wartość
+
 function between (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+function getNumberOfQuestions() {
+    return parseInt(prompt('How many questions d0 yuo want'));
+}
+let numberOfQuestions = getNumberOfQuestions();
+while (isNaN(numberOfQuestions)) {
+    numberOfQuestions = getNumberOfQuestions();
+}
+
 
 let correctAnswersNumb = 0;
 
