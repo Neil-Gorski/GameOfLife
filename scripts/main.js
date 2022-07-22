4957ce2c52b002670a6edafcecf8f012bbe09d70
Neil Gorski <neil.gorski@hotmail.ch>
2022-07-22 22:38:22 +0200

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -57,19 +57,50 @@
 
 const testArray = ['Szymon', 'Adrian', 'Kasia', 1, 10, undefined, null, NaN, ['kraków', 'Warszawa'], 'ala', 'basia', 20, 25, 13, 'ola', 'alan', "50", "1", "13"];
 
-//Nowa tablica z imiona dłuższymi niż 5 znaków
-//Nowa tablica z numberami do kwadratu
-//nowa tablica tylko number - NumberArray
 //nowa tablica bez wartości negatywnych
-//nowa tablica ze wszystkimi elementami typu string + jesli element zaczyna się z małej litery to zmieniamy ją na wielką
 
 
+//nowa tablica tylko number - NumberArray - typ number i string!!!
 function filterNumbers(element) {
-    if(typeof element === 'number' && isNaN(element) !== true){
+    if((typeof element === 'number' && isNaN(element) !== true || typeof element === 'string'&& !isNaN(element))){
         return true;
     }
 }
+const numberArray = testArray.filter(filterNumbers);
+//console.log(numberArray);
 
-const numberArray = testArray.filter(filterNumbers)
 
-console.log(numberArray)
+
+//Nowa tablica z numberami do kwadratu
+function squareNumbers(element){
+    return element**2;
+}
+const numberSquares = numberArray.map(squareNumbers);
+//console.log(numberSquares)
+
+
+
+//Nowa tablica z imiona dłuższymi niż 5 znaków
+function fitlerLongNames(element){
+    if(typeof element === 'string' && element.length > 5){
+        return true
+    }
+}
+const longNames = testArray.flat().filter(fitlerLongNames)
+//console.log(longNames)
+
+
+
+//nowa tablica ze wszystkimi elementami typu string + jesli element zaczyna się z małej litery to zmieniamy ją na wielką
+function isString(element){
+    if(typeof element === 'string'&& !isNaN(element)){
+        return true;
+    }
+}
+function upperStrings(element){
+    if(typeof element === 'string'){
+        return element.charAt(0).toUpperCase() + element.substring(1).toLowerCase();
+    }
+}
+const upperStringsArr = testArray.flat().filter(isString).map(upperStrings)
+console.log(upperStringsArr);
