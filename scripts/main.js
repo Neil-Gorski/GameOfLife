25cad69867807992d28c4719309dfa42a9f92ba7
Neil Gorski <neil.gorski@hotmail.ch>
2022-07-29 23:41:32 +0200

    exercise-05 - creat html interface bugfix with fuel usage

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,27 @@
+const btnAccelerate = document.querySelector(".controlPanel__Accelerate");
+const btnDecelerate = document.querySelector(".controlPanel__Decelerate");
+const btnDrive = document.querySelector(".controlPanel__Drive");
+const btnRefuel = document.querySelector(".controlPanel__Refuel");
+
 class Car {
     constructor(name, maximumSpeed, color="white", mileage=0, fuelTank=60) {
         this.name = this.setStringValue(name, 'Default Name');
         this.maximumSpeed = this.setNumberValue(maximumSpeed, 200);
         this.color = color;
-        this.speed = 0;             // km/h
+        this.speed = 0;                // km/h
         this.numberOfWheels = 4;
         this.numberOfGears = 8;
-        this.fuelTank = fuelTank;   // liter
+        this.fuelTank = fuelTank;      // liter
         this.fuelAmount = this.fuelTank;    //liter
-        this.fuelConsum = 0.5;      // l/km
+        this.fuelConsum = 0.08;         // l/km
         this.haveFuel = true;
-        this.mileage = mileage;     // m
-        this.carAcceleration = 3.1    // m/s²
+        this.mileage = mileage;        // m
+        this.carAcceleration = 3.1;    // m/s²
+        this.carDeceleration = 6;      // m/s²
+        this.carInfo()
     }
+
+    carInfo = () => console.log(`###############\nInfo\nName : ${this.name} \nColor: ${this.color} \nFuel: ${this.fuelAmount} \nMileage: ${this.mileage}\n###############`)
 
     setStringValue = (value, defaultValue) => {
         return typeof value !== 'string' || value.length === 0 ? defaultValue : value;
@@ -25,14 +34,14 @@ class Car {
     setColor(color) {
         if (typeof color === 'string') {
             this.color = color;
-            console.log(`The new color of ${this.name} is ${this.color}.`)
+            console.log(`The new color of ${this.name} is ${this.color}.`);
         } else {
             throw new Error("Color value should be string");
         }
     }
 
     getColor = () => {
-        console.log(`The color of ${this.name} is ${this.color}.`)
+        console.log(`The color of ${this.name} is ${this.color}.`);
         return this.color;
     }
 
@@ -52,73 +61,97 @@ class Car {
 
     accelerate = (howMuch) => {
         if(this.haveFuel){
-            let distance = 0
+            let distance = 0;
             const newHowMuch = this.setNumberValue(howMuch, 0);
             newHowMuch < 0 ? 0 : newHowMuch;
-            console.log("-----------------------------")
-            console.log(`${this.name} is accelerating. The current speed: ${this.speed}.`)
-            this.setFuelUsed(howMuch)
-            if (this.haveFuel) {distance = this.accelerateDistance(newHowMuch)}; 
-            if (this.haveFuel) {this.setSpeed(this.speed + newHowMuch)};
-            this.mileage += distance;
-            if (this.haveFuel) {console.log(`The car needed ${distance.toFixed(2)} meter for acceleration.`)}
-            if (this.haveFuel == false) {console.log(`The car run out of fuel... no accelaration posible.`)};
+            console.log("-----------------------------");
+            console.log(`${this.name} is accelerating. The current speed: ${this.speed} km/h.`);
+            if (this.speed === this.maximumSpeed){
+                console.log("Car drive already with top speed!")
+            }else{
+                this.setFuelUsed(howMuch, false);
+                if (this.haveFuel) {distance = this.accelerateDistance(newHowMuch, this.carAcceleration)}; 
+                if (this.haveFuel) {this.setSpeed(this.speed + newHowMuch)};
+                this.mileage += distance;
+                if (this.haveFuel) {console.log(`The car needed ${distance.toFixed(2)} meter for acceleration.`)};
+                if (this.haveFuel == false) {console.log(`The car run out of fuel... no accelaration posible.`)};
+                this.getMilage();
+            }
         }else{
-            console.log("-----------------------------")
-            console.log(`You try to accelerate.`)
-            console.log(`${this.name} is out of fuel, fill up the fuel !!! `)
+            console.log("-----------------------------");
+            console.log(`You try to accelerate.`);
+            console.log(`${this.name} is out of fuel, fill up the fuel !!! `);
         }
-        console.log(`Current mileage: ${this.getMilage().toFixed(2)} meter.`)
     }
 
     decelerate = (howMuch) => {
         const newHowMuch = this.setNumberValue(howMuch,0);
         newHowMuch > 0 ? newHowMuch : 0;
-        console.log("-----------------------------")
-        console.log(`${this.name} is decelerating.`)
-        this.setSpeed(this.speed - newHowMuch);
+        let distance = 0;
+        console.log("-----------------------------");
+        if(this.speed === 0){
+            console.log("Car is already standing!")
+        }else{
+            console.log(`${this.name} is decelerating. The current speed: ${this.speed} km/h.`);
+            distance = this.accelerateDistance(newHowMuch, this.carDeceleration)
+            this.setSpeed(this.speed - newHowMuch);
+            this.mileage += distance;
+            console.log(`The car needed ${distance.toFixed(2)} meter for deceleration.`)
+            this.getMilage();  
+        }
     }
 
-    setFuelUsed = (howMuch) => {
-        const fuelUsed = this.fuelConsum * (howMuch > this.maximumSpeed - this.speed ? this.maximumSpeed - this.speed : howMuch)
+    setFuelUsed = (howMuch, drive) => {
+        const fuelMultiplikator = drive ? 1/40 * ((this.speed + 1)**0.5) : 1/80 * ((this.speed + 1)**0.5); 
+        
+        const fuelUsed = (this.fuelConsum * fuelMultiplikator) * (howMuch > this.maximumSpeed - this.speed ? this.maximumSpeed - this.speed : howMuch);
         this.fuelAmount = this.fuelAmount - fuelUsed;
         if (this.fuelAmount < 0 ){
             this.fuelAmount = 0;
             this.haveFuel = false;
             console.log(`${this.name} is out of fuel`);
         }else{
-            console.log(`For this acceleration you used ${fuelUsed} liter fuel.`);
+            console.log(`Car used ${fuelUsed} liter fuel.`);
         }
         console.log(`${this.fuelAmount.toFixed(2)} liter fuel left over.`);
     }
 
     fillUpFuel = (howMuch) => {
-        if(howMuch > 0){
-            console.log("-----------------------------")
-            console.log(`You fill up fuel:`)
+        if(howMuch > 0 && this.speed === 0){
+            console.log("-----------------------------");
+            console.log(`You fill up fuel:`);
             this.haveFuel = true;
             const oldFuelAmount = this.fuelAmount;
-            const howMuchFilldUp = howMuch > (this.fuelTank - this.fuelAmount) ? this.fuelTank : howMuch;
+            const howMuchFilldUp = howMuch > (this.fuelTank - this.fuelAmount) ? this.fuelTank - this.fuelAmount : howMuch;
             this.fuelAmount += howMuchFilldUp; 
-            console.log(`You try to fill ${howMuch}l. But ther is only space for ${howMuchFilldUp}l.`);
-            console.log(`${this.name} had ${oldFuelAmount}l and you fill up ${howMuchFilldUp}l, so now are ${this.fuelAmount}l in the tank.`);
+            console.log(`You try to fill ${howMuch}l.`);
+            console.log(`${this.name} had ${oldFuelAmount.toFixed(2)}l and you fill up ${howMuchFilldUp.toFixed(2)}l, so now are ${this.fuelAmount.toFixed(2)}l in the tank.`);
+        }else{
+            console.log("-----------------------------");
+            console.log("You need first stop the car to fuel up.")
         }
     }
 
-    accelerateDistance = (deltaV) => {  //s = 0,5 · a · t2 + vo · t + s.
-        const time = (deltaV / 3.6) / this.carAcceleration;
+    accelerateDistance = (deltaV, accelaration) => {  //s = 0,5 · a · t2 + vo · t + s.
+        const time = (deltaV / accelaration) / this.carAcceleration;
         const distance = (0.5 * this.carAcceleration * time**2) + ((this.speed / 3.6) * time); // speed in m/s 
-        return distance   // in meter
+        return distance;   // in meter
     }
 
     drive = (time) => {
-        this.mileage = time * (this.speed / 3.6); 
-        console.log("-----------------------------")
-        console.log(`Car drives ${time}s with ${this.speed}km/h.`)
-        console.log(`Current mileage: ${this.getMilage().toFixed(2)}m`);
+        if (this.speed === 0){
+            console.log("You need first speed up to drive.)")
+        }else{
+            const distance = time * (this.speed / 3.6);
+            this.mileage += distance; 
+            console.log("-----------------------------");
+            console.log(`Car drives ${time}s with ${this.speed}km/h.`);
+            this.setFuelUsed(distance, true);
+            console.log(`Current mileage: ${this.mileage.toFixed(2)}m`);
+        }
     }
 
-    getMilage = () => this.mileage;
+    getMilage = () => console.log(`Current mileage: ${this.mileage.toFixed(2)} meter.`); 
 };
 
 // const firstCar = new Car("BMW", 180);
@@ -138,10 +171,10 @@ class Car {
 //     new Car("")
 // ]
 const mojeAuto = new Car("Fiat", 190);
-mojeAuto.accelerate(50);
-mojeAuto.accelerate(50);
-mojeAuto.drive(460);
-mojeAuto.accelerate(50);
+// mojeAuto.accelerate(50);
+// mojeAuto.accelerate(50);
+// mojeAuto.drive(460);
+// mojeAuto.accelerate(50);
 // mojeAuto.accelerate(50);
 // mojeAuto.accelerate(50);
 // mojeAuto.accelerate(50);
@@ -155,3 +188,7 @@ mojeAuto.accelerate(50);
 //mojeAuto.setColor("pink")
 
 // console.log(cars)
+btnAccelerate.addEventListener("click", function (){mojeAuto.accelerate(20)});
+btnDecelerate.addEventListener("click", function (){mojeAuto.decelerate(40)});
+btnDrive.addEventListener("click", function () {mojeAuto.drive(600)});
+btnRefuel.addEventListener("click", function () {mojeAuto.fillUpFuel(15)});
