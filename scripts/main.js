6753b89b93191bad83a01c3244973887251cf642
Neil Gorski <neil.gorski@hotmail.ch>
2022-07-29 16:32:07 +0200

    exercise-05-fuel

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,17 @@
 class Car {
-    constructor(name, maximumSpeed, color="white") {
+    constructor(name, maximumSpeed, color="white", mileage=0, fuelTank=60) {
         this.name = this.setStringValue(name, 'Default Name');
         this.maximumSpeed = this.setNumberValue(maximumSpeed, 200);
         this.color = color;
-        this.speed = 0;
+        this.speed = 0;             // km/h
         this.numberOfWheels = 4;
         this.numberOfGears = 8;
+        this.fuelTank = fuelTank;   // liter
+        this.fuelAmount = this.fuelTank;    //liter
+        this.fuelConsum = 0.5;      // l/km
+        this.haveFuel = true;
+        this.mileage = mileage;     // m
+        this.carAcceleration = 5    // km/h²
     }
 
     setStringValue = (value, defaultValue) => {
@@ -29,6 +35,7 @@ class Car {
         console.log(`The color of ${this.name} is ${this.color}.`)
         return this.color;
     }
+
     setSpeed = (speed) => {
         if (speed > this.maximumSpeed){
             this.speed = this.maximumSpeed;
@@ -39,22 +46,63 @@ class Car {
         }
         // this.speed = speed > this.maximumSpeed ? this.maximumSpeed : speed;
         // this.speed = speed < 0 ? this.minimumSpeed : speed;
-        console.log(`${this.name} drive with ${this.speed} km/h`);
+        console.log(`${this.name} drive with ${this.speed} km/h.`);
+        
     }
 
     accelerate = (howMuch) => {
-        const newHowMuch = this.setNumberValue(howMuch, 0);
-        newHowMuch < 0 ? 0 : newHowMuch;
-        this.setSpeed(this.speed + newHowMuch);
+        if(this.haveFuel){
+            const newHowMuch = this.setNumberValue(howMuch, 0);
+            newHowMuch < 0 ? 0 : newHowMuch;
+            console.log("-----------------------------")
+            console.log(`${this.name} is accelerating.`)
+            this.setFuelUsed(howMuch)
+            this.setSpeed(this.speed + newHowMuch);
+        }else{
+            console.log("-----------------------------")
+            console.log(`You try to accelerate.`)
+            console.log(`${this.name} is out of fuel, fill up the fuel !!! `)
+        }
     }
 
     decelerate = (howMuch) => {
         const newHowMuch = this.setNumberValue(howMuch,0);
         newHowMuch > 0 ? newHowMuch : 0;
+        console.log("-----------------------------")
+        console.log(`${this.name} is decelerating.`)
         this.setSpeed(this.speed - newHowMuch);
     }
 
-}
+    setFuelUsed = (howMuch) => {
+        const fuelUsed = this.fuelConsum * (howMuch > this.maximumSpeed - this.speed ? this.maximumSpeed - this.speed : howMuch)
+        this.fuelAmount = this.fuelAmount - fuelUsed;
+        if (this.fuelAmount < 0 ){
+            this.fuelAmount = 0;
+            this.haveFuel = false;
+            console.log(`${this.name} is out of fuel`);
+        }else{
+            console.log(`For this acceleration you used ${fuelUsed} liter fuel.`);
+        }
+        console.log(`${this.fuelAmount.toFixed(2)} liter fuel left over.`);
+    }
+
+    fillUpFuel = (howMuch) => {
+        if(howMuch > 0){
+            console.log("-----------------------------")
+            console.log(`You fill up fuel:`)
+            this.haveFuel = true;
+            const oldFuelAmount = this.fuelAmount;
+            const howMuchFilldUp = howMuch > (this.fuelTank - this.fuelAmount) ? this.fuelTank : howMuch;
+            this.fuelAmount += howMuchFilldUp; 
+            console.log(`You try to fill ${howMuch}l. But ther is only space for ${howMuchFilldUp}l.`);
+            console.log(`${this.name} had ${oldFuelAmount}l and you fill up ${howMuchFilldUp}l, so now are ${this.fuelAmount}l in the tank.`);
+        }
+    }
+    accelerateDistance = (howMuch) => {
+
+    }
+    
+};
 
 const firstCar = new Car("BMW", 180);
 firstCar.setColor("black");
@@ -74,17 +122,18 @@ secCar.getColor();
 // ]
 const mojeAuto = new Car("Fiat", 190);
 mojeAuto.accelerate(50);
-mojeAuto.accelerate(60);
-mojeAuto.accelerate(100);
-mojeAuto.decelerate(50);
+mojeAuto.accelerate(50);
+mojeAuto.accelerate(50);
+mojeAuto.accelerate(50);
+mojeAuto.accelerate(50);
+mojeAuto.accelerate(50);
+mojeAuto.accelerate(50);
 mojeAuto.decelerate(40);
 mojeAuto.accelerate(20);
-mojeAuto.decelerate(200);
-mojeAuto.accelerate(200);
-mojeAuto.decelerate(40);
-mojeAuto.decelerate(500);
-mojeAuto.accelerate(5000);
-mojeAuto.getColor();
-mojeAuto.setColor("pink")
+mojeAuto.fillUpFuel(40);
+mojeAuto.accelerate(40);
+mojeAuto.fillUpFuel(200);
+//mojeAuto.getColor();
+//mojeAuto.setColor("pink")
 
 // console.log(cars)
