f583db09424d9486d8aff3c3b747679a4352905b
Neil Gorski <neil.gorski@hotmail.ch>
2022-08-23 09:27:05 +0200

    checkAmountOfNeighours

--- scripts/utilis.js
+++ scripts/utilis.js
@@ -8,10 +8,9 @@ export const timer = ms => new Promise(res => setTimeout(res, ms))
 export const clone = (items) => items.map(item => Array.isArray(item) ? clone(item) : item);
 
 export function getCordinateFromString (str){
-  let cordinates
-  let arr = str.split(" ");
-  if(arr[0] === "field-square"){
-    cordinates = arr[1].split("-");
+  let cordinates;
+  if (str !== ""){
+    cordinates = str.split("-");
     return cordinates
   }
   return null
